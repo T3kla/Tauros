560145427a60761daa6d98c0aacdfcc53a416d26
T3kla <manuillo94@gmail.com>
2023-01-28 17:03:13 +0100

    Sending stuff to tauri command :')

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,14 +4,14 @@
 )]
 
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn send_command(dest: &str, file: &str, sess: &str, mode: &str, name: &str, desc: &str) -> String {
     println!("I was invoked from JS, with this message",);
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("{} {} {} {} {} {}", dest, file, sess, mode, name, desc)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![send_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app.rs
+++ src/app.rs
@@ -14,77 +14,96 @@ extern "C" {
 }
 
 #[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
+struct SendArgs<'a> {
+    dest: &'a str,
+    file: &'a str,
+    sess: &'a str,
+    mode: &'a str,
     name: &'a str,
+    desc: &'a str,
 }
 
-struct Model(i32);
+#[derive(Serialize, Deserialize, PartialEq)]
+struct Counter(u32);
 
 #[function_component(App)]
 pub fn app() -> Html {
     let dest_input_ref = use_node_ref();
     let file_input_ref = use_node_ref();
-    let session_input_ref = use_node_ref();
+    let sess_input_ref = use_node_ref();
     let mode_input_ref = use_node_ref();
     let name_input_ref = use_node_ref();
     let desc_input_ref = use_node_ref();
 
-    // states
-    //      sets up a state that yew is aware of, so it can re-render the component when it changes
-    let state = use_state(|| Model(2));
-    let name = use_state(|| String::new());
-    let greet_msg = use_state(|| String::new());
+    let response_back = use_state(|| Counter { 0: 0u32 });
+    let response_front = use_state(|| String::new());
 
-    // functions
-    //      functions that can be called from html
-    //      this functions can change states but not execute rust code
-
-    // returns the data from greet_input_ref
-    let greet = {
-        let name = name.clone();
-        let greet_input_ref = dest_input_ref.clone();
+    // when send button is pressed, response_back state is changed
+    let on_send_pressed = {
+        let response_back = response_back.clone();
 
         Callback::from(move |_| {
-            name.set(
-                greet_input_ref
-                    .cast::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value(),
-            );
+            response_back.set(Counter {
+                0: response_back.0 + 1,
+            });
         })
     };
 
-    // function that adds 1 to the state
-    let on_click = {
-        // creates a clone of state because the original state is being used by the UI framework
-        let state = state.clone();
-
-        // move -> moves the ownership of stuff the closure uses to the closure and makes them inaccessible outside
-        // |_|  -> callback expects a function that takes a parameter, but we don't need it
-        Callback::from(move |_| state.set(Model { 0: state.0 * 2 }))
-    };
-
-    // *******************************
-
+    // hooks to the response_back state invokes a command when it changes
     {
-        let greet_msg = greet_msg.clone();
-        let name = name.clone();
-        let name2 = name.clone();
+        let response_front = response_front.clone();
+        let response_back = response_back.clone();
+        let response_back2 = response_back.clone();
+
+        let dest_input_ref = dest_input_ref.clone();
+        let file_input_ref = file_input_ref.clone();
+        let sess_input_ref = sess_input_ref.clone();
+        let mode_input_ref = mode_input_ref.clone();
+        let name_input_ref = name_input_ref.clone();
+        let desc_input_ref = desc_input_ref.clone();
+
         use_effect_with_deps(
             move |_| {
                 spawn_local(async move {
-                    if name.is_empty() {
+                    if response_back.0 == 0 {
                         return;
                     }
-                    let new_msg =
-                        invoke("greet", to_value(&GreetArgs { name: &*name }).unwrap()).await;
-                    log(&new_msg.as_string().unwrap());
-                    greet_msg.set(new_msg.as_string().unwrap());
+                    let new_msg = invoke(
+                        "send_command",
+                        to_value(&SendArgs {
+                            dest: &dest_input_ref
+                                .cast::<web_sys::HtmlInputElement>()
+                                .unwrap()
+                                .value(),
+                            file: &file_input_ref
+                                .cast::<web_sys::HtmlInputElement>()
+                                .unwrap()
+                                .value(),
+                            sess: &sess_input_ref
+                                .cast::<web_sys::HtmlInputElement>()
+                                .unwrap()
+                                .value(),
+                            mode: &mode_input_ref
+                                .cast::<web_sys::HtmlInputElement>()
+                                .unwrap()
+                                .value(),
+                            name: &name_input_ref
+                                .cast::<web_sys::HtmlInputElement>()
+                                .unwrap()
+                                .value(),
+                            desc: &desc_input_ref
+                                .cast::<web_sys::HtmlInputElement>()
+                                .unwrap()
+                                .value(),
+                        })
+                        .unwrap(),
+                    )
+                    .await;
+                    response_front.set(new_msg.as_string().unwrap());
                 });
-
                 || {}
             },
-            name2,
+            response_back2,
         );
     }
 
@@ -93,7 +112,7 @@ pub fn app() -> Html {
 
             <div class="row">
                 <label id="label" for="input">{"Destination:"}</label>
-                <input id="input" ref={dest_input_ref} value="\\\\192.168.1.40\\Developers\\SHARE AND DELETE\\____LOGS" />
+                <input id="input" ref={dest_input_ref} placeholder="\\\\192.168.1.40\\Developers\\SHARE AND DELETE\\____LOGS" />
             </div>
 
             <div class="row">
@@ -103,7 +122,7 @@ pub fn app() -> Html {
 
             <div class="row">
                 <label id="label" for="input">{"Session:"}</label>
-                <input id="input" ref={session_input_ref} placeholder="00" />
+                <input id="input" ref={sess_input_ref} placeholder="00" />
             </div>
 
             <div class="row">
@@ -122,10 +141,8 @@ pub fn app() -> Html {
             </div>
 
             <div class="row">
-                <button type="button" onclick={greet}>{"Send"}</button>
-                <button type="button" onclick={on_click}>{" +1 "}</button>
-                <p><center><b>{ state.0 }</b></center></p>
-                <p><center><b>{ &*greet_msg }</b></center></p>
+                <button type="button" onclick={on_send_pressed}>{"Send"}</button>
+                <p id="response">{ &*response_front }</p>
             </div>
 
         </main>
